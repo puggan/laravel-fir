557b9c2fee96144410ece7096022b657f1385986
Thorbjörn Sundragon <puggan@spiro.se>
2018-10-06 03:18:10 +0200

    All errors as json under /api/

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,4 +6,11 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+    public function render($request, \Exception $e)
+    {
+        if(strpos($request->path(), 'api/') === 0) {
+            $request->headers->set('accept', 'application/json', true);
+        }
+        return parent::render($request, $e);
+    }
 }
